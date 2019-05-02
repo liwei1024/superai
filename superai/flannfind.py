f8110b9539807dab1d0b11a98f2d7b052c0e857c
yqsy <yqsy021@126.com>
2019-05-02 15:38:36 +0800

    重构一下

--- superai/flannfind.py
+++ superai/flannfind.py
@@ -2,55 +2,22 @@ import sys
 import os
 import time
 
+import numpy as np
+
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../'))
 
 import cv2
 
 from superai.screenshots import WindowCaptureToMem
 
+MIN_MATCH_COUNT = 10
 
 def main():
     if len(sys.argv) < 2:
         print("usage: {} png".format(sys.argv[0]))
         exit(0)
 
-    findImg = sys.argv[1]
-
-    sift = cv2.xfeatures2d.SIFT_create()
-
-    findImg = cv2.imread(findImg)
-    findKp, findDes = sift.detectAndCompute(findImg, None)
-
-    while True:
-        sceneImg = WindowCaptureToMem("地下城与勇士", "地下城与勇士")
-        sceneKp, scensDes = sift.detectAndCompute(sceneImg, None)
-
-        indexParams = dict(algorithm=0, trees=5)
-        searchParams = dict(checks=50)
-        flann = cv2.FlannBasedMatcher(indexParams, searchParams)
-
-        matches = flann.knnMatch(findDes, scensDes, k=2)
-        matchesMask = [[0, 0] for i in range(len(matches))]
-
-        for i, (m, n) in enumerate(matches):
-            if m.distance < 0.7 * n.distance:
-                matchesMask[i] = [1, 0]
-
-        drawParams = dict(matchColor=(0, 255, 0),
-                          singlePointColor=(255, 0, 0),
-                          matchesMask=matchesMask,
-                          flags=0)
-
-        resultImg = cv2.drawMatchesKnn(findImg, findKp, sceneImg, sceneKp, matches, None, **drawParams)
-
-        cv2.imshow('my img', resultImg)
-
-        if (cv2.waitKey(30) & 0xFF) in [ord('q'), 27]:
-            break
-
-        time.sleep(0.1)
-
-    cv2.destroyAllWindows()
+    # img1 = cv2.imread(sys.argv[1], 0)
 
 
 if __name__ == "__main__":
--- superai/orbfind.py
+++ superai/orbfind.py
@@ -14,30 +14,6 @@ def main():
         print("usage: {} png".format(sys.argv[0]))
         exit(0)
 
-    findImg = sys.argv[1]
-
-    orb = cv2.ORB_create()
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-
-    findImg = cv2.imread(findImg)
-    findKp, findDes = orb.detectAndCompute(findImg, None)
-
-    while True:
-        sceneImg = WindowCaptureToMem("地下城与勇士", "地下城与勇士")
-        sceneKp, scensDes = orb.detectAndCompute(sceneImg, None)
-
-        matches = bf.match(findDes, scensDes)
-        matches = sorted(matches, key=lambda x: x.distance)
-        resultImg = cv2.drawMatches(findImg, findKp, sceneImg, sceneKp, matches[:25], sceneImg, flags=2)
-
-        cv2.imshow('my img', resultImg)
-
-        if (cv2.waitKey(30) & 0xFF) in [ord('q'), 27]:
-            break
-
-        time.sleep(0.1)
-
-    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
