01ad0b5c2948e87227c9e05ffbe51605f3d3a64e
yqsy <yqsy021@126.com>
2019-05-02 15:55:47 +0800

    这个还可以

--- superai/flannfind.py
+++ superai/flannfind.py
@@ -4,6 +4,8 @@ import time
 
 import numpy as np
 
+from matplotlib import pyplot as plt
+
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../'))
 
 import cv2
@@ -12,12 +14,69 @@ from superai.screenshots import WindowCaptureToMem
 
 MIN_MATCH_COUNT = 10
 
+
 def main():
     if len(sys.argv) < 2:
         print("usage: {} png".format(sys.argv[0]))
         exit(0)
 
-    # img1 = cv2.imread(sys.argv[1], 0)
+    while True:
+        img1 = cv2.imread(sys.argv[1], 0)
+        img2 = WindowCaptureToMem("地下城与勇士", "地下城与勇士")
+
+        # Initiate SIFT detector
+        sift = cv2.xfeatures2d.SIFT_create()
+
+        # find the keypoints and descriptors with SIFT
+        kp1, des1 = sift.detectAndCompute(img1, None)
+        kp2, des2 = sift.detectAndCompute(img2, None)
+
+        FLANN_INDEX_KDTREE = 0
+        index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+        search_params = dict(checks=50)
+
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+        matches = flann.knnMatch(des1, des2, k=2)
+
+        # store all the good matches as per Lowe's ratio test.
+        good = []
+        for m, n in matches:
+            if m.distance < 0.7 * n.distance:
+                good.append(m)
+
+        if len(good) > MIN_MATCH_COUNT:
+            src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+            dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+
+            M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+            matchesMask = mask.ravel().tolist()
+
+            h, w = img1.shape
+            pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+            dst = cv2.perspectiveTransform(pts, M)
+
+            img2 = cv2.polylines(img2, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
+
+        else:
+            print("Not enough matches are found - %d/%d" % (len(good), MIN_MATCH_COUNT))
+            matchesMask = None
+
+        draw_params = dict(matchColor=(0, 255, 0),  # draw matches in green color
+                           singlePointColor=None,
+                           matchesMask=matchesMask,  # draw only inliers
+                           flags=2)
+
+        img3 = cv2.drawMatches(img1, kp1, img2, kp2, good, None, **draw_params)
+
+        cv2.imshow('my img', img3)
+
+        if (cv2.waitKey(30) & 0xFF) in [ord('q'), 27]:
+            break
+
+        time.sleep(0.1)
+
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
