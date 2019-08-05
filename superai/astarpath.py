22ab51cf7585ab8e28a05e5e7e2d1407fa03f0ab
yq <yqsy021@126.com>
2019-08-05 16:59:36 +0800

    作画吧

---
+++ superai/astarpath.py
@@ -0,0 +1,199 @@
+import os
+import sys
+
+import cv2
+import numpy as np
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../'))
+
+# a* 8方位
+import copy
+
+from superai.astartdemo import manhattanDistance, idxTohw, hwToidx
+from superai.common import Log
+from superai.gameapi import FlushPid, GameApiInit, GetMenInfo
+from superai.obstacle import GetGameObstacleData, drawAll
+
+
+class AStartPaths:
+    def __init__(self, d, start, end):
+        # 初始化
+        # 1. 地形二叉树 2. 地形数组 3. 地形额外  使用 16 * 12 长宽的格子
+        # 4. 障碍物  使用特定长宽的格子
+        self.mapCellWLen = d.mapw // 0x10
+        self.mapCellHLen = d.maph // 0xc
+        self.cellnum = self.mapCellWLen * self.mapCellHLen
+        self.dixing = [False] * self.cellnum
+
+        Log("self.mapCellWLen: %d self.mapCellHLen: %d \n" % (self.mapCellWLen, self.mapCellHLen))
+
+        for obj in d.dixingtree:
+            cellX = obj.x // 0x10
+            cellY = obj.y // 0xc
+            self.dixing[cellY * self.mapCellWLen + cellX] = True
+
+        for obj in d.dixingvec:
+            cellX = obj.x // 0x10
+            cellY = obj.y // 0xc
+            self.dixing[cellY * self.mapCellWLen + cellX] = True
+
+        for obj in d.dixingextra:
+            cellX = obj.x // 0x10
+            cellY = obj.y // 0xc
+            self.dixing[cellY * self.mapCellWLen + cellX] = True
+
+        self.obstacles = copy.deepcopy(d.obstacles)
+
+        # 初始化 A * 算法数据
+        # start end && 行走的路径使用 10 * 10 长宽的格子
+        # 缺点是有误差 水平4个点, 垂直8个点
+        self.manCellHWLen = d.mapw // 10
+        self.manCellWLen = d.maph // 10
+        self.manCellnum = self.manCellHWLen * self.manCellWLen
+
+        self.closedSet = []
+        self.openSet = [start]
+
+        self.start = start
+        self.end = end
+
+        self.edgeTo = [0] * self.manCellnum
+        self.marked = [False] * self.manCellnum
+
+        # 实际距离
+        self.gScore = [0] * self.manCellnum
+        self.gScore[start] = 0
+
+        # 估算到终点的距离
+        self.fScore = [0] * self.manCellnum
+        self.fScore[start] = manhattanDistance(idxTohw(start, self.manCellWLen), idxTohw(end, self.manCellWLen))
+
+        self.astar()
+
+    def ObstacleTouched(self, x, y):
+        for v in self.obstacles:
+            # 坐标在障碍物的范围之内
+            halfw = int(v.w / 2)
+            halfh = int(v.h / 2)
+            if (v.x - halfw < x < v.x + halfw) and \
+                    (v.y - halfh < y < v.y + halfh):
+                return True
+
+        return False
+
+    def GetAdjs(self, pos):
+        # 获取八方位邻居格子. 根据地形和障碍数据过滤掉不必要的
+        adjs = []
+
+        posx, posy = idxTohw(pos, self.manCellHWLen)
+
+        # 上下左右. 左上,左下,右上,右下.
+        checks = [
+            (posx, posy - 1),
+            (posx, posy + 1),
+            (posx - 1, posy),
+            (posx + 1, posy),
+            (posx - 1, posy - 1),
+            (posx - 1, posy + 1),
+            (posx + 1, posy - 1),
+            (posx + 1, posy + 1),
+        ]
+
+        for (adjx, adjy) in checks:
+            # 1. 地形. 矩形的中点不在格子内
+            # 把10 x 10 格子的中点 转换成 0xc 0x10 的格子. 判断是否可移动
+            adjx2 = (adjx * 10 + 5) // 0x10
+            adjy2 = (adjy * 10 + 5) // 0xc
+            if not self.dixing[hwToidx(adjx2, adjy2, self.mapCellWLen)]:
+                # 2. 障碍物. 矩形的中点和不在障碍物内
+                # 把10 x 10 格子的中点 转换成坐标中点.
+                adjx3 = adjx * 10 + 5
+                adjy3 = adjy * 10 + 5
+                if not self.ObstacleTouched(adjx3, adjy3):
+                    adjs.append(hwToidx(adjx, adjy, self.manCellWLen))
+
+        return adjs
+
+    def astar(self):
+        while len(self.openSet) > 0:
+            current = min(self.openSet, key=lambda s: self.fScore[s])
+            if current == self.end:
+                return
+            self.openSet.remove(current)
+            self.closedSet.append(current)
+            for w in self.GetAdjs(current):
+                if w in self.closedSet:
+                    continue
+                # 实际距离
+                tentativeScore = self.gScore[current] + manhattanDistance(idxTohw(current, self.manCellWLen),
+                                                                          idxTohw(w, self.manCellWLen))
+                if w not in self.openSet:
+                    self.openSet.append(w)
+                elif tentativeScore >= self.gScore[w]:  # 如果此遍历距离大于其他点遍历过去的距离则抛弃
+                    continue
+
+                self.edgeTo[w] = current
+                self.marked[w] = True
+
+                print("edgeTo ({},{}) -> ({},{})".format(current % self.manCellWLen, current // self.manCellWLen,
+                                                         w % self.manCellWLen, w // self.manCellWLen))
+
+                self.gScore[w] = tentativeScore
+                self.fScore[w] = self.gScore[w] + manhattanDistance(idxTohw(w, self.manCellWLen),
+                                                                    idxTohw(self.end, self.manCellWLen))
+                print("fScore[%d] manhattan: %d" % (w, self.fScore[w]))
+
+    def HasPathTo(self, v: int):
+        return self.marked[v]
+
+    def PathTo(self, v: int):
+        result = []
+        if not self.HasPathTo(v):
+            return result
+
+        x = v
+        while x != self.start:
+            result.append(x)
+            x = self.edgeTo[x]
+        result.append(self.start)
+        return result
+
+
+def main():
+    if GameApiInit():
+        print("Init helpdll-xxiii.dll ok")
+    else:
+        print("Init helpdll-xxiii.dll err")
+        exit(0)
+    FlushPid()
+
+    # (647,312,0) 血滴石
+
+    d = GetGameObstacleData()
+
+    img = np.zeros((d.maph, d.mapw, 3), dtype=np.uint8)
+    img[np.where(img == [0])] = [255]
+
+    drawAll(img, d)
+
+    wlen = d.mapw // 10
+    meninfo = GetMenInfo()
+
+    begincellx, begincelly = int(meninfo.x) // 10, int(meninfo.y) // 10
+    endcellx, endcelly = 647 // 10, 312 // 10
+    startidx = hwToidx(begincellx, begincelly, wlen)
+    endidx = hwToidx(endcellx, endcelly, wlen)
+
+    print("a* 寻径")
+    astar = AStartPaths(d, startidx, endidx)
+    paths = astar.PathTo(hwToidx(endcellx, endcelly, wlen))
+    for v in reversed(paths):
+        print(v, idxTohw(v, d.mapw // 10))
+
+    cv2.imshow('img', img)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
--- superai/obstacle.py
+++ superai/obstacle.py
@@ -27,77 +27,94 @@ def GetGameObstacleData():
     return GameObstacleData(wh.w, wh.h, dixingtree, dixingvec, dixingextra, obstacles)
 
 
+def drawDixing(img, d):
+    # 地形二叉树. x,y左上角
+    for v in d.dixingtree:
+        cv2.rectangle(img, (v.x, v.y), (v.x + 0x10, v.y + 0xc), (144, 128, 112), 2)
+
+    # 地形数组. x,y左上角
+    for v in d.dixingvec:
+        cv2.rectangle(img, (v.x, v.y), (v.x + 0x10, v.y + 0xc), (64, 64, 64), 2)
+
+    # 地形额外. x,y左上角
+    for v in d.dixingextra:
+        cv2.rectangle(img, (v.x, v.y), (v.x + 0x10, v.y + 0xc), (64, 64, 64), 2)
+
+
+def drawObstacles(img, d):
+    # 障碍物. x,y中点
+    for v in d.obstacles:
+        halfw = int(v.w / 2)
+        halfh = int(v.h / 2)
+
+        # 障碍物可以被攻击
+        if v.CanBeAttack() > 0:
+            cv2.rectangle(img, (v.x - halfw, v.y - halfh), (v.x + halfw, v.y + halfh), (0, 255, 0), 2)
+        else:
+            cv2.rectangle(img, (v.x - halfw, v.y - halfh), (v.x + halfw, v.y + halfh), (64, 64, 64), 2)
+
+
+def drawOther(img):
+    # 人,怪物,物品. x,y中点
+    meninfo = GetMenInfo()
+    halfw = int(meninfo.w / 2)
+    halfh = int(meninfo.h / 2)
+    cv2.rectangle(img, (int(meninfo.x) - halfw, int(meninfo.y) - halfh),
+                  (int(meninfo.x) + halfw, int(meninfo.y) + halfh),
+                  (0, 0, 255), 2)
+
+    monsters = GetMonsters()
+    for mon in monsters:
+        halfw = int(mon.w / 2)
+        halfh = int(mon.h / 2)
+        cv2.rectangle(img, (int(mon.x) - halfw, int(mon.y) - halfh),
+                      (int(mon.x) + halfw, int(mon.y) + halfh),
+                      (0, 140, 255), 2)
+
+    goods = GetGoods()
+    for good in goods:
+        halfw = int(20 / 2)
+        halfh = int(20 / 2)
+        cv2.rectangle(img, (int(good.x) - halfw, int(good.y) - halfh),
+                      (int(good.x) + halfw, int(good.y) + halfh),
+                      (0, 140, 255), 2)
+
+
+def drawDoor(img):
+    # 门. x,y 左上角
+    nextdoor = GetNextDoor()
+    cv2.rectangle(img, (nextdoor.x, nextdoor.y), (nextdoor.x + nextdoor.w, nextdoor.y + nextdoor.h),
+                  (255, 144, 30), 2)
+
+
+def drawLine(img, d):
+    for i in range(d.mapw // 10):
+        cv2.line(img, (i * 10, 0), (i * 10, d.maph), (144, 128, 112), 1)
+
+    for i in range(d.maph // 10):
+        cv2.line(img, (0, i * 10), (d.mapw, i * 10), (144, 128, 112), 1)
+
+
+def drawAll(img, d):
+    drawDixing(img, d)
+    drawObstacles(img, d)
+    drawOther(img)
+    drawDoor(img)
+    drawLine(img, d)
+
+
 def loop():
     while True:
         if IsManInMap():
 
-            dixingtree, dixingvec, dixingextra, obstacles, wh = GetSeceneInfo()
-
-            if wh.h != 0 and wh.w != 0:
-                img = np.zeros((wh.h, wh.w, 3), dtype=np.uint8)
+            d = GetGameObstacleData()
+            if d.maph != 0 and d.mapw != 0:
+                img = np.zeros((d.maph, d.mapw, 3), dtype=np.uint8)
             else:
                 img = np.zeros((1, 1, 3), dtype=np.uint8)
-
             img[np.where(img == [0])] = [255]
-            # print("宽高: %s" % wh)
 
-            # cv2.rectangle(img, (1, 1), (wh.w - 1, wh.len_c * 0xc - 1),
-            #               (147, 20, 255), 2)
-
-            # cv2.rectangle(img, (1, wh.len_c * 0xc + 1), (wh.w - 1, wh.len_c * 0xc + wh.len_extra_78 * 0x78 - 1),
-            #               (147, 20, 255), 2)
-
-            # 地形二叉树. x,y左上角
-            for v in dixingtree:
-                cv2.rectangle(img, (v.x, v.y), (v.x + 0x10, v.y + 0xc), (144, 128, 112), 2)
-
-            # 地形数组. x,y左上角
-            for v in dixingvec:
-                cv2.rectangle(img, (v.x, v.y), (v.x + 0x10, v.y + 0xc), (64, 64, 64), 2)
-
-            # 地形额外. x,y左上角
-            for v in dixingextra:
-                cv2.rectangle(img, (v.x, v.y), (v.x + 0x10, v.y + 0xc), (64, 64, 64), 2)
-
-            # 障碍物. x,y中点
-            for v in obstacles:
-                halfw = int(v.w / 2)
-                halfh = int(v.h / 2)
-
-                # 障碍物可以被攻击
-                if v.CanBeAttack() > 0:
-                    cv2.rectangle(img, (v.x - halfw, v.y - halfh), (v.x + halfw, v.y + halfh), (0, 255, 0), 2)
-                else:
-                    cv2.rectangle(img, (v.x - halfw, v.y - halfh), (v.x + halfw, v.y + halfh), (64, 64, 64), 2)
-
-            # 人,怪物,物品. x,y中点
-            meninfo = GetMenInfo()
-            halfw = int(meninfo.w / 2)
-            halfh = int(meninfo.h / 2)
-            cv2.rectangle(img, (int(meninfo.x) - halfw, int(meninfo.y) - halfh),
-                          (int(meninfo.x) + halfw, int(meninfo.y) + halfh),
-                          (0, 0, 255), 2)
-
-            monsters = GetMonsters()
-            for mon in monsters:
-                halfw = int(mon.w / 2)
-                halfh = int(mon.h / 2)
-                cv2.rectangle(img, (int(mon.x) - halfw, int(mon.y) - halfh),
-                              (int(mon.x) + halfw, int(mon.y) + halfh),
-                              (0, 140, 255), 2)
-
-            goods = GetGoods()
-            for good in goods:
-                halfw = int(20 / 2)
-                halfh = int(20 / 2)
-                cv2.rectangle(img, (int(good.x) - halfw, int(good.y) - halfh),
-                              (int(good.x) + halfw, int(good.y) + halfh),
-                              (0, 140, 255), 2)
-
-            # 门. x,y 左上角
-            nextdoor = GetNextDoor()
-            cv2.rectangle(img, (nextdoor.x, nextdoor.y), (nextdoor.x + nextdoor.w, nextdoor.y + nextdoor.h),
-                          (255, 144, 30), 2)
+            drawAll(img, d)
 
             cv2.imshow('img', img)
             if (cv2.waitKey(30) & 0xFF) in [ord('q'), 27]:
