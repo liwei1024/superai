3d470f7c83c61cb7d7fa4d4d94638748394c8530
yq <yqsy021@126.com>
2019-10-28 23:06:15 +0800

    截图

--- superai/screenshots.py
+++ superai/screenshots.py
@@ -2,7 +2,12 @@ import sys
 import os
 import time
 
+import win32con
+import win32ui
+
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../'))
+
+from superai.defer import defer
 
 import logging
 
@@ -12,35 +17,10 @@ import cv2
 import numpy as np
 import win32gui
 import datetime
-from PIL import ImageGrab
 
 
-def WindowCaptureToFile(windowClassName, windowName, captureDir, dx=0, dy=0, dw=0, dh=0):
-    hwnd = win32gui.FindWindow(windowClassName, windowName)
-    if hwnd == 0:
-        return None
-    left, top, right, bot = win32gui.GetWindowRect(hwnd)
-
-    if dx != 0:
-        left = left + dx
-
-    if dy != 0:
-        top = top + dy
-
-    if dw != 0:
-        right = left + dw
-
-    if dh != 0:
-        bot = top + dh
-
-    img = ImageGrab.grab((left, top, right, bot),  all_screens=True)
-
-    filename = '{}/screenshot{}.png'.format(captureDir, datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
-    img.save(filename)
-    return filename
-
-
-def WindowCaptureToMem(windowClassName, windowName, dx=0, dy=0, dw=0, dh=0):
+@defer
+def WindowCaptureToFile(windowClassName, windowName, captureDir, dx=0, dy=0, dw=0, dh=0, defer=None):
     hwnd = win32gui.FindWindow(windowClassName, windowName)
     if hwnd == 0:
         return None
@@ -59,15 +39,70 @@ def WindowCaptureToMem(windowClassName, windowName, dx=0, dy=0, dw=0, dh=0):
     if dh != 0:
         bot = top + dh
 
-    img = ImageGrab.grab((left, top, right, bot), all_screens=True)
-    img = np.array(img)
+    hdesktop = win32gui.GetDesktopWindow()
+    desktopDC = win32gui.GetWindowDC(hdesktop)
+    defer(lambda: (win32gui.ReleaseDC(hdesktop, desktopDC)))
 
-    try:
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-    except TypeError:
-        img = np.zeros((h, w, 3), dtype=np.uint8)
+    imgDC = win32ui.CreateDCFromHandle(desktopDC)
+    defer(lambda: (imgDC.DeleteDC()))
 
-    return img
+    memDC = imgDC.CreateCompatibleDC()
+    defer(lambda: (memDC.DeleteDC()))
+
+    bitmap = win32ui.CreateBitmap()
+    defer(lambda: (win32gui.DeleteObject(bitmap.GetHandle())))
+
+    bitmap.CreateCompatibleBitmap(imgDC, w, h)
+    oldmap = memDC.SelectObject(bitmap)
+    memDC.BitBlt((0, 0), (w, h), imgDC, (left, top), win32con.SRCCOPY)
+
+    filename = '{}/screenshot{}.png'.format(captureDir, datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
+    bitmap.SaveBitmapFile(memDC, filename)
+    memDC.SelectObject(oldmap)
+    return filename
+
+
+@defer
+def WindowCaptureToMem(windowClassName, windowName, dx=0, dy=0, dw=0, dh=0, defer=None):
+    hwnd = win32gui.FindWindow(windowClassName, windowName)
+    if hwnd == 0:
+        return None
+    left, top, right, bot = win32gui.GetWindowRect(hwnd)
+    w, h = right - left, bot - top
+
+    if dx != 0:
+        left = left + dx
+
+    if dy != 0:
+        top = top + dy
+
+    if dw != 0:
+        right = left + dw
+
+    if dh != 0:
+        bot = top + dh
+
+    hdesktop = win32gui.GetDesktopWindow()
+    desktopDC = win32gui.GetWindowDC(hdesktop)
+    defer(lambda: (win32gui.ReleaseDC(hdesktop, desktopDC)))
+
+    imgDC = win32ui.CreateDCFromHandle(desktopDC)
+    defer(lambda: (imgDC.DeleteDC()))
+
+    memDC = imgDC.CreateCompatibleDC()
+    defer(lambda: (memDC.DeleteDC()))
+
+    bitmap = win32ui.CreateBitmap()
+    defer(lambda: (win32gui.DeleteObject(bitmap.GetHandle())))
+
+    bitmap.CreateCompatibleBitmap(imgDC, w, h)
+    oldmap = memDC.SelectObject(bitmap)
+    memDC.BitBlt((0, 0), (w, h), imgDC, (left, top), win32con.SRCCOPY)
+
+    npbytes = np.frombuffer(bitmap.GetBitmapBits(True), dtype='uint8')
+    npbytes.shape = (h, w, 4)
+    memDC.SelectObject(oldmap)
+    return cv2.cvtColor(npbytes, cv2.COLOR_BGRA2BGR)
 
 
 def main():
@@ -76,7 +111,7 @@ def main():
     # cv2.waitKey()
     # cv2.destroyAllWindows()
 
-    img = WindowCaptureToMem("TWINCONTROL", "WeGame")
+    img = WindowCaptureToMem("地下城与勇士", "地下城与勇士")
 
     cv2.imshow('my img', img)
     cv2.waitKey()
